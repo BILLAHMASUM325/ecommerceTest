79d9a67dc7bd46ed2b3b46060bf197bbd36050ec
Masum Billah <57219534+BILLAHMASUM325@users.noreply.github.com>
2023-09-14 01:29:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from "cors";
 import path from 'path'
+import {fileURLToPath} from 'url';
 
 
 
@@ -22,6 +23,8 @@ dotenv.config();
 connectDB();
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename);
 //rest object
 const app = express()
 
